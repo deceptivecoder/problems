0e23b95dbc5e5c3bfd780692cc3ec187053c3a25
kaushal billore <kbillore@gmail.com>
2022-10-19 21:51:29 +0530

    Array: containsDuplicate

---
+++ array/containsDuplicate.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    //TLE: O(n^2) Space O(1)
+    // bool containsDuplicate(vector<int>& nums) {
+    //     int size = nums.size();
+    //     bool result = false;
+    //     for (int i =0; i<=size-2; i++){
+    //         for(int j = i+1; j <=size-1;j++){
+    //             if(!(nums[i]^nums[j]))
+    //                 return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    //Time O(nlogn) Space O(1)
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+        bool result = false;
+        for(int i = 1; i<n ; i++){
+            if(nums[i] == nums[i-1]){
+                result = true;
+            }
+        }
+        return result;
+    }
+};
+
+int main(){
+    Solution ob;
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(0);
+    
+    
+    cout << ob.containsDuplicate(nums) <<endl;
+    return 0;
+}
