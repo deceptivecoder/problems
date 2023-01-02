6d408087b79998891751508b76f68c9bb531c87e
kaushal billore <kbillore@gmail.com>
2023-01-02 17:06:57 +0530

    string: find substring sliding window

--- string/strStr.cpp
+++ string/strStr.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    //T: O(n2) S : O(1)
+    //T: O(n) S : O(1) //Sliding Window
     int strStr(string haystack, string needle) {
         int sLen = haystack.size();
         int subLen = needle.size();
-        for (int sitr = 0; sitr < sLen; sitr++ ){
-            for (int subitr = 0; subitr < subLen; subitr++ ) {
-                if (haystack[sitr+subitr] != needle[subitr]){
+        for (int i = 0; i < sLen; i++ ){
+            for (int j = 0; j < subLen; j++ ) {
+                if (haystack[i+j] != needle[j]){
                     break;
                 }
-                if (subitr == needle.length() -1) {
-                    return sitr;
+                if (j == needle.length() -1) {
+                    return j;
                 }
             }
         }
         return -1;
     }
 };
+
 
 int main(){
     Solution ob;
